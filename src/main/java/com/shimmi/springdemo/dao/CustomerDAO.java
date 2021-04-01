b219584585158be14e236458f54b95689e3d3376
prawinkandulna <prawinkandulna@ameyo.com>
2021-04-01 09:58:16 +0530

    CRM-To get all customers

---
+++ src/main/java/com/shimmi/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.shimmi.springdemo.dao;
+
+import java.util.List;
+
+import com.shimmi.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
